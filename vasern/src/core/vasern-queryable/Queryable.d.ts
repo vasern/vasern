728f5adf878e1ac14d64b088110beafed1ca3f3a
curtisy <lausklaus44@gmail.com>
2019-02-08 23:29:55 +0100

    Add typings for queryable

---
+++ vasern/src/core/vasern-queryable/Queryable.d.ts
@@ -0,0 +1,30 @@
+declare module "Queryable" {
+    export interface Query {
+        id: string;
+    }
+
+    export default class Queryable {
+        // TODO: Add a more descriptive type from usages
+        constructor(data: ArrayLike<any>): Queryable;
+
+        data(): ArrayLike<any>;
+
+        _removeRecord(query: Query | string): any[];
+
+        get(query: Query | string): any | undefined;
+
+        find(query: Query): any | undefined;
+
+        filter(query: Query): Queryable;
+
+        similarTo(props: string, value: string): any[];
+
+        exclude(query: Query): Queryable;
+
+        group(key: string, transform?: (key: string, data: any) => any): any[] | Queryable;
+
+        order(key: string, order: "asc" | "desc" = "asc"): Queryable;
+
+        count(): number;
+    }
+}
