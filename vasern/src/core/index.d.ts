c8a2d91b5437a19ec1720df473ca6fc974c8356a
Hieu Nguyen (Jack) <nc.hieu@live.com>
2019-02-16 15:52:38 +1100

    Added type definition for "core" directory

---
+++ vasern/src/core/index.d.ts
@@ -0,0 +1,6 @@
+import Queryable from "./vasern-queryable";
+import { Vasern, Document } from "./vasern";
+import Parser from "./vasern-parser";
+import EventSubscriber from "./vasern-subscriber";
+
+export { Vasern, Document, Queryable, Parser, EventSubscriber };
