fc23847446e94e59f733db806e0fff60c4141162
apppro123 <buchavm@hotmail.com>
2020-12-04 23:46:24 +0100

    Revert "fixed typo"
    This reverts commit 8f3c1a4ad28cd575c3bad50f4c3dd80d33fec649.

--- android/src/main/java/com/reactlibrary/RNVasernUtils.java
+++ android/src/main/java/com/reactlibrary/RNVasernUtils.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 class VasernUtils {
 
-    public static WritableArray listToWritableArray(List<String> inputs) {
+    public static WritableArray listToWriableArray(List<String> inputs) {
 
         WritableArray data = Arguments.createArray(); 
     
